31b0dd8ed09f14e887a9a3318a180d96d936f6e2
tomnijhof <tom.nijhof@cytosmart.com>
2020-09-11 14:04:24 +0200

    update integration to incluse serial numbers

--- integration.py
+++ integration.py
@@ -5,10 +5,15 @@ from luxconnector import LuxConnector
 
 connector = LuxConnector()
 
-# s = time.time()
-# img = connector.get_image()
-# print(time.time() - s)
+serial_numbers = connector.get_all_serial_numbers()
+
+if len(serial_numbers) > 0:
+    s = time.time()
+    img = connector.get_image(serial_numbers[0])
+    print(time.time() - s)
+else:
+    print("No devices are connected")
 
 s = time.time()
-z_stack = connector.get_z_stack(10, 0, 1)
+z_stack = connector.get_z_stack(serial_numbers[0], 10, 0, 1)
 print(time.time() - s)
