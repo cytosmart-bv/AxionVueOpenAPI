0216ce64f7af184afffcd795ad54fd317c33ba10
tomnijhof <tom.nijhof@cytosmart.com>
2020-09-14 12:45:14 +0200

    time single image

--- integration.py
+++ integration.py
@@ -9,11 +9,12 @@ from luxconnector import LuxConnector
 result_folder = os.path.join("results", "single_image")
 os.makedirs(result_folder, exist_ok=True)
 
-connector = LuxConnector()
+connector = LuxConnector(wait_till_a_device_connects=True)
 
 serial_numbers = connector.get_all_serial_numbers()
 
 for serial_number in serial_numbers:
+    s = time.time()
     img = connector.get_image(serial_number)
-
+    print(f"total time {time.time() - s}")
     img.save(os.path.join(result_folder, f"{serial_number}_{int(time.time())}.png"), "JPEG")
