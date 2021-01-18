be4b732cfc38c007e8daee8c86ec589deb5697bc
Karina Kozarova <karinakozarova1303@gmail.com>
2021-01-18 09:54:34 +0100

    bumped version

--- luxconnector/__init__.py
+++ luxconnector/__init__.py
@@ -2,7 +2,7 @@
 """This is a python wrapper around the Lux Client windows solution"""
 
 __author__ = """Tom Nijhof"""
-__version__ = "1.0.0"
+__version__ = "1.0.0b1"
 
 # Add here import to all the functions you need
 from .main import LuxConnector
