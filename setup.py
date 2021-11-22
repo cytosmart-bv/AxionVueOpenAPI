98229cd38633450b01f0cbd9331d58828769311a
tomnijhof <tom.nijhof@cytosmart.com>
2021-11-22 17:29:42 +0100

    update version number
    Signed-off-by: tomnijhof <tom.nijhof@cytosmart.com>

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/cytosmart-bv/luxconnector",
-    version="1.0.3b0",
+    version="1.0.3",
     zip_safe=False,
 )
