b0e20bdaf39a2d54844f6f37fb38cb16c313d01d
tomnijhof <tom.nijhof@cytosmart.com>
2021-12-03 13:32:39 +0100

    Bump version: 1.1.0 → 1.2.0b0

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/cytosmart-bv/luxconnector",
-    version="1.1.0",
+    version="1.2.0b0",
     zip_safe=False,
 )
