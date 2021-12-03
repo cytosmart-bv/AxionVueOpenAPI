d2bd2e9947f1ff0c102d5f989d275842a768acda
tomnijhof <tom.nijhof@cytosmart.com>
2021-12-03 14:05:25 +0100

    make integration witdher in use
    Signed-off-by: tomnijhof <tom.nijhof@cytosmart.com>

--- integration_fluo.py
+++ integration_fluo.py
@@ -16,11 +16,11 @@ s = time.time()
 connector.set_camera_settings(serial_number, "BRIGHTFIELD", 10)
 connector.set_active_camera(serial_number, "BRIGHTFIELD")
 img = connector.get_image(serial_number)
-img.save(os.path.join(result_folder, f"{serial_number}_BRIGHTFIELD_1.jpg"), "JPEG")
+img.save(os.path.join(result_folder, f"{serial_number}_BRIGHTFIELD_.jpg"), "JPEG")
 
 # RED
 s = time.time()
-connector.set_camera_settings(serial_number, "RED", 500)
+connector.set_camera_settings(serial_number, "RED", 500, gain=30, brightness=5000)
 connector.set_active_camera(serial_number, "RED")
 img = connector.get_image(serial_number)
 img.save(os.path.join(result_folder, f"{serial_number}_RED.jpg"), "JPEG")
@@ -34,7 +34,7 @@ img.save(os.path.join(result_folder, f"{serial_number}_GREEN.jpg"), "JPEG")
 
 # GREEN z-stack
 connector.set_active_camera(serial_number, "GREEN")
-z_stack = connector.get_z_stack(serial_number, start_focus=0.1, stop_focus=0.2)
+z_stack = connector.get_z_stack(serial_number, num_img=10 ,start_focus=0.1, stop_focus=0.2)
 
 for idx, img in enumerate(z_stack):
     img.save(os.path.join(result_folder, f"{serial_number}_GREEN_z{str(idx).zfill(3)}.jpg"), "JPEG")
