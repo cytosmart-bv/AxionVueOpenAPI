a55f53d7d824089a186fabac8fe07b20d811b8e4
tomnijhof <tom.nijhof@cytosmart.com>
2022-02-18 09:23:12 +0100

    update pillow requirements
    Signed-off-by: tomnijhof <tom.nijhof@cytosmart.com>

--- setup.py
+++ setup.py
@@ -14,12 +14,8 @@ with open("HISTORY.rst") as history_file:
 with open("LICENSE") as license_file:
     license = license_file.read()
 
-requirements = [
-    "pillow>=6.2.2, <9",
-    "requests>=2.24.0, <3",
-    "websocket>=0.2.1, <0.3",
-    "websocket-client>=0.57.0, <0.59",
-]
+with open("requirements.txt") as requirements_file:
+    requirements = requirements_file.read().split("\n")
 
 setup_requirements = ["pytest-runner"]
 
