846e6fb2f2baa384f3330953483b65aaaa6dd6a2
tomnijhof <tom.nijhof@cytosmart.com>
2022-07-22 15:20:49 +0200

    Bump version: 1.1.1 → 1.1.2b0

--- luxconnector/__init__.py
+++ luxconnector/__init__.py
@@ -2,7 +2,7 @@
 """This is a python wrapper around the Lux Client windows solution"""
 
 __author__ = """Tom Nijhof"""
-__version__ = "1.1.1"
+__version__ = "1.1.2b0"
 
 # Add here import to all the functions you need
 from .main import LuxConnector
