ecdc6a13982331f1a13fbe64ba4d3d671af04feb
tomnijhof <tom.nijhof@cytosmart.com>
2022-07-28 09:24:50 +0200

    Bump version: 1.2.0b1 → 1.2.0

--- CytoSmartOpenAPI/__init__.py
+++ CytoSmartOpenAPI/__init__.py
@@ -2,7 +2,7 @@
 """This is a python wrapper for the CytoSMART windows app to use it headless"""
 
 __author__ = """Tom Nijhof"""
-__version__ = "1.2.0b1"
+__version__ = "1.2.0"
 
 # Add here import to all the functions you need
 from .main import CytoSmartOpenAPI
