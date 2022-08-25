7ac7c597a33fc0031b0c1afec97fd5492de21bcd
tomnijhof <tom.nijhof@cytosmart.com>
2022-08-25 11:33:29 +0200

    add integration file
    Signed-off-by: tomnijhof <tom.nijhof@cytosmart.com>

---
+++ integration_auto_focus.py
@@ -0,0 +1,14 @@
+#%%
+from CytoSmartOpenAPI import CytoSmartOpenAPI
+
+
+connector = CytoSmartOpenAPI(number_of_devices=1, warranty=False)
+
+serial_number = connector.get_all_serial_numbers()[0]
+
+
+connector.set_liveview(serial_number, True)
+connector.open_liveview(
+    serial_number,
+)
+connector.do_autofocus(serial_number, "cytoSmart slide")
